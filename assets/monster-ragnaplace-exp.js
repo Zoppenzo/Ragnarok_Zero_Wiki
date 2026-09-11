cdab80a9d9cee2d635eac97c56c157efdc43d9fc
Zoppenzo <enzo.olympio27@gmail.com>
2026-09-11 03:09:32 +0200

    Remove single-source RagnaPlace EXP overlay

--- assets/monster-ragnaplace-exp.js
+++
@@ -1,7 +0,0 @@
-(() => {
-  'use strict';
-  const data={};
-  const meta={source:'RagnaPlace ROZg EN',generatedAt:null,targets:0,pages:0,baseExp:0,jobExp:0};
-  window.RZ_MONSTER_RAGNAPLACE_EXP=data;
-  window.RZ_MONSTER_RAGNAPLACE_EXP_META=meta;
-})();
