5521e5c571a835b8ba721c27211e8ad07a0fe7ee
Zoppenzo <enzo.olympio27@gmail.com>
2026-09-11 02:52:45 +0200

    Add RagnaPlace Zero EXP overlay placeholder

---
+++ assets/monster-ragnaplace-exp.js
@@ -0,0 +1,7 @@
+(() => {
+  'use strict';
+  const data={};
+  const meta={source:'RagnaPlace ROZg EN',generatedAt:null,targets:0,pages:0,baseExp:0,jobExp:0};
+  window.RZ_MONSTER_RAGNAPLACE_EXP=data;
+  window.RZ_MONSTER_RAGNAPLACE_EXP_META=meta;
+})();
