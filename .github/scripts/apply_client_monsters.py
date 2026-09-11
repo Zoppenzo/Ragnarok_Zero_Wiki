cb7689770257064360701b5a5a203e0c0fe81364
Zoppenzo <enzo.olympio27@gmail.com>
2026-09-11 02:09:28 +0200

    Bust monster element asset cache

--- .github/scripts/apply_client_monsters.py
+++ .github/scripts/apply_client_monsters.py
@@ -4,7 +4,7 @@ import re
 p=Path('index.html')
 s=p.read_text(encoding='utf-8')
 
-version='20260910-monster-current-navi4'
+version='20260911-element-matrix1'
 script_paths=[
     'assets/client-monsters-data.js',
     'assets/client-monster-disable-legacy-maps.js',
